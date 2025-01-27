86e2442fc3ea7b62e78da13de3cb42b14fc1b372
vsevex <vsevex@gmail.com>
2025-01-27 11:39:29 +0400

    add deployment script for Tonite contract

---
+++ scripts/deployTonite.ts
@@ -0,0 +1,13 @@
+import { toNano } from '@ton/core';
+import { Tonite } from '../wrappers/Tonite';
+import { compile, NetworkProvider } from '@ton/blueprint';
+
+export async function run(provider: NetworkProvider) {
+    const tonite = provider.open(Tonite.createFromConfig({}, await compile('Tonite')));
+
+    await tonite.sendDeploy(provider.sender(), toNano('0.05'));
+
+    await provider.waitForDeploy(tonite.address);
+
+    // run methods on `tonite`
+}
