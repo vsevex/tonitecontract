c10c2bd6665109989cdea7f76182f2c3c0e46ee2
vsevex <vsevex@gmail.com>
2025-01-31 18:55:26 +0400

    configure Jest to use a single worker for test execution

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
     preset: 'ts-jest',
     testEnvironment: 'node',
+    maxWorkers: 1,
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
 
