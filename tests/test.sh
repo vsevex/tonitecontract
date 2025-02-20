68065da559e073cea02653bad872f9849ca5d604
vsevex <vsevex@gmail.com>
2025-02-20 18:48:10 +0400

    refactor: remove obsolete key creation and test scripts

--- tests/test.sh
+++
@@ -1,6 +0,0 @@
-../contracts/build.sh
-
-echo "\nCompilation completed\n"
-
-export FIFTPATH=~/TON/ton/crypto/fift/lib
-~/TON/build/crypto/fift -I ~/TON/ton/crypto/fift/lib -s ./tonite-test-suite.fif
